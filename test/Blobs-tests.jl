ce981e0f6a712e1aef00d955fc05a64ae273c9a1
Bradley C. Kuszmaul <bradley.kuszmaul@relational.ai>
2025-11-24 13:41:11 +0000

    fix test and implement the view properly

--- test/Blobs-tests.jl
+++ test/Blobs-tests.jl
@@ -75,6 +75,7 @@ if Base.JLOptions().check_bounds == 0
 end
 
 # view
+bv = BlobVector{Int64}(data, 3)
 bv[1] = 101
 bv[2] = 102
 bv[3] = 103
@@ -85,6 +86,7 @@ bv[3] = 103
 @test view(bv, 2:3) == [102,103]
 @test view(bv, 3:3) == [103]
 @test view(bv, 4:3) == []
+@info bv
 for lo in 1:4
     for hi in 0:3
         @test @view(bv[lo:hi]) == @view([101,102,103][lo:hi])
